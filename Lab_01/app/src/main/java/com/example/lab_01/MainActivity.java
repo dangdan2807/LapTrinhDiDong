2205bd425c8a3d121083757e5710f3a8d589e479
KayJuno <dangdan2807@gmail.com>
2022-02-18 01:25:01 +0700

    update Lap_01: add layout password generator and layout profile

--- Lab_01/app/src/main/java/com/example/lab_01/MainActivity.java
+++ Lab_01/app/src/main/java/com/example/lab_01/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnLayoutA, btnLayoutB, btnLayoutC, btnLayoutD, btnLayoutE;
-    private Button btnLayoutComments;
+    private Button btnLayoutComments, btnLayoutTiki, btnLayoutProfile, btnLayoutPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         btnLayoutC = findViewById(R.id.layoutMain_btnLayoutC);
         btnLayoutD = findViewById(R.id.layoutMain_btnLayoutD);
         btnLayoutE = findViewById(R.id.layoutMain_btnLayoutE);
-        btnLayoutComments = findViewById(R.id.layoutMain_btnComment);
+        btnLayoutComments = findViewById(R.id.layoutMain_btnLayoutComment);
+        btnLayoutTiki = findViewById(R.id.layoutMain_btnLayoutTiki);
+        btnLayoutProfile = findViewById(R.id.layoutMain_btnLayoutProfile);
+        btnLayoutPassword = findViewById(R.id.layoutMain_btnLayoutPassword);
         setEventClick();
     }
 
@@ -68,6 +71,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, Comments.class);
+                startActivity(i);
+            }
+        });
+        btnLayoutPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, PasswordGenerator.class);
+                startActivity(i);
+            }
+        });
+        btnLayoutProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Profile.class);
+                startActivity(i);
+            }
+        });
+        btnLayoutTiki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Tiki.class);
                 startActivity(i);
             }
         });
