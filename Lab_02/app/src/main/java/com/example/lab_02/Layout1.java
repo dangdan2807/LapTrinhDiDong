fc7b72b9d636ddc8199b1842440706f55a8d5322
KayJuno <dangdan2807@gmail.com>
2022-02-22 10:48:21 +0700

    add Lab_02
    - add layout1

--- Lab_02/app/src/main/java/com/example/lab_02/Layout1.java
+++ Lab_02/app/src/main/java/com/example/lab_02/Layout1.java
@@ -1,28 +1,28 @@
-package com.example.lab_01;
+package com.example.lab_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Layout_1 extends AppCompatActivity implements View.OnClickListener {
-    private Button btnLogin, btnSignUp;
+public class Layout1 extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLogin, btnForgotPassword;
     private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout1);
-
-        btnLogin = findViewById(R.id.layout1_btnLogin);
-        btnSignUp = findViewById(R.id.layout1_btnSignUp);
         context = this;
+        btnLogin = findViewById(R.id.layout1_btnLogin);
+        btnForgotPassword = findViewById(R.id.layout1_btnForgotPassword);
+
         btnLogin.setOnClickListener(this);
-        btnSignUp.setOnClickListener(this);
+        btnForgotPassword.setOnClickListener(this);
     }
 
     @Override
---
+++ Lab_02/app/src/main/java/com/example/lab_02/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lab_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLayoutA, btnLayoutB, btnLayoutC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLayoutA = findViewById(R.id.layoutMain_btn1);
+        btnLayoutB = findViewById(R.id.layoutMain_btn2);
+        btnLayoutC = findViewById(R.id.layoutMain_btn3);
+        btnLayoutA.setOnClickListener(this);
+        btnLayoutB.setOnClickListener(this);
+        btnLayoutC.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Object o = view;
+        if (o.equals(btnLayoutA)) {
+            Intent i = new Intent(MainActivity.this, Layout1.class);
+            startActivity(i);
+        } else if (o.equals(btnLayoutB)) {
+
+        } else if (o.equals(btnLayoutC)) {
+
+        }
+    }
+}
