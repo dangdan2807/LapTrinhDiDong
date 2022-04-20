68e4d07b33c29489c57b735eabd52ac0dbc6c816
KayJuno <dangdan2807@gmail.com>
2022-04-20 15:17:01 +0700

    update lab_08_firebase: add future login, update status, sign up

--- Lab_08_Firebase/app/src/main/java/com/example/lab_08_firebase/RegisterScreen.java
+++ Lab_08_Firebase/app/src/main/java/com/example/lab_08_firebase/RegisterScreen.java
@@ -1,29 +1,54 @@
 package com.example.lab_08_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterScreen extends AppCompatActivity {
     private EditText edtName, edtEmail, edtPassword, edtRePassword;
     private Button btnSignUp, btnRegisterGoogle, btnSignIn;
+    private Context context;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        context = this;
 
-        edtName = findViewById(R.id.RegisterA_edtName);
-        edtEmail = findViewById(R.id.RegisterA_edtEmail);
-        edtPassword = findViewById(R.id.RegisterA_edtPassword);
-        edtRePassword = findViewById(R.id.RegisterA_edtRePassword);
-        btnSignUp = findViewById(R.id.RegisterA_btnSignUp);
-        btnRegisterGoogle = findViewById(R.id.RegisterA_btnRegisterGoogle);
-        btnSignIn = findViewById(R.id.RegisterA_btnSignIn);
+        database = FirebaseDatabase.getInstance();
+
+        edtName = findViewById(R.id.registerA_edtName);
+        edtEmail = findViewById(R.id.registerA_edtEmail);
+        edtPassword = findViewById(R.id.registerA_edtPassword);
+        edtRePassword = findViewById(R.id.registerA_edtRePassword);
+
+        btnSignUp = findViewById(R.id.registerA_btnSignUp);
+        btnRegisterGoogle = findViewById(R.id.registerA_btnRegisterGoogle);
+        btnSignIn = findViewById(R.id.registerA_btnSignIn);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean result = register();
+                if (result) {
+                    Toast.makeText(context, "Đăng ký tài khoản thành công hãy đăng nhập", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +57,48 @@ public class RegisterScreen extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private boolean register() {
+        String name = edtName.getText().toString().trim();
+        String email = edtEmail.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+        String rePassword = edtRePassword.getText().toString().trim();
+        if (name.isEmpty() || name.length() <= 0) {
+            Toast.makeText(context, "Name không được để trống", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (email.isEmpty() || email.length() <= 0) {
+            Toast.makeText(context, "Email không được để trống", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (password.isEmpty() || password.length() <= 0) {
+            Toast.makeText(context, "Password không được để trống", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (rePassword.isEmpty() || rePassword.length() <= 0) {
+            Toast.makeText(context, "Password nhập lại không được để trống", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (!password.equals(rePassword)) {
+            Toast.makeText(context, "Password không giống nhau", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        DatabaseReference myRef = database.getReference("users");
+        User user = new User(name, email, password);
+        myRef.setValue(user);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User theUser = snapshot.getValue(User.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return true;
     }
 }
--- Lab_08_Firebase/app/src/main/java/com/example/lab_08_firebase/SignInScreen.java
+++ Lab_08_Firebase/app/src/main/java/com/example/lab_08_firebase/SignInScreen.java
@@ -2,27 +2,69 @@ package com.example.lab_08_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignInScreen extends AppCompatActivity {
     private Button btnSignIn, btnForgotPassword, btnSignInGoogle, btnRegister;
     private EditText edtEmail, edtPassword;
+    private Context context;
+    private FirebaseDatabase database;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+        context = this;
+
+        database = FirebaseDatabase.getInstance();
 
         btnSignIn = findViewById(R.id.signInA_btnSignIn);
         btnForgotPassword = findViewById(R.id.signInA_btnForgotPassword);
         btnSignInGoogle = findViewById(R.id.signInA_btnSignInGoogle);
         btnRegister = findViewById(R.id.signInA_btnRegister);
+
         edtEmail = findViewById(R.id.signInA_edtEmail);
         edtPassword = findViewById(R.id.signInA_edtPassword);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+                boolean result = checkData(email, password);
+                if(result) {
+                    DatabaseReference myRef = database.getReference("users");
+                    myRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            user = dataSnapshot.getValue(User.class);
+                            Toast.makeText(context, "Đăng nhập thành công", Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(SignInScreen.this, FaceScreen.class);
+                            i.putExtra("user", user);
+                            startActivity(i);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            Toast.makeText(context, "Đăng nhập thất bại", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +73,17 @@ public class SignInScreen extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    public boolean checkData(String email, String password) {
+        if(email.isEmpty() || email.length() <= 0) {
+            Toast.makeText(context, "Email không được để trống", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(password.isEmpty() || password.length() <= 0) {
+            Toast.makeText(context, "Mật khẩu không được để trống", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
